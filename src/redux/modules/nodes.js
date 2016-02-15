2b193d2e66f6e44fd8245bbbd90f704461c94582
Daniel Ingraham <daniel@danielingraham.com>
2016-02-15 16:09:28 -0500

    0001 - whip node reducer and action creators

--- src/redux/modules/nodes.js
+++ src/redux/modules/nodes.js
@@ -11,11 +11,23 @@ export default function reducer(state = initialState, action = {}) {
         ...state,
         expanded: true
       };
+    case COLLAPSE_NODE:
+      return {
+        ...state,
+        expanded: false
+      };
     default:
       return state;
   }
 };
 
-export function isExpanded() {
-  
+export function isExpanded(globalState, nodeId) {
+  return globalState.nodes[nodeId] && globalState.nodes[nodeId].expanded;
 };
+
+export function expand(nodeId) {
+  return {
+    type: EXPAND_NODE,
+    nodeId
+  };
+}
