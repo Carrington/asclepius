c5caf8a2f5670603abaac8fdf8aa2dceb80d9bd7
Daniel Ingraham <daniel@danielingraham.com>
2016-02-15 17:01:10 -0500

    0001 - node load reducers and action creator

--- src/redux/modules/nodes.js
+++ src/redux/modules/nodes.js
@@ -1,3 +1,6 @@
+const LOAD = 'asclepius/node/LOAD';
+const LOAD_SUCCESS = 'asclepius/node/LOAD_SUCCESS';
+const LOAD_FAIL = 'asclepius/node
 const EXPAND_NODE = 'asclepius/node/EXPAND';
 
 const initialState = {
@@ -5,19 +8,50 @@ const initialState = {
 };
 
 export default function reducer(state = initialState, action = {}) {
+  let newState = {};
+  newState.nodes = state.nodes;
+
   switch (action.type) {
     case EXPAND_NODE:
+      newState.nodes[action.nodeId].expanded = true;
       return {
         ...state,
-        expanded: true
+        newState
       };
+    break;
     case COLLAPSE_NODE:
+      newState.nodes[action.nodeId].expanded = false;
       return {
         ...state,
-        expanded: false
+        newState
       };
+    break;
+    case LOAD:
+      newState.nodes[action.nodeId].loading = true;
+      return {
+        ...state,
+        newState
+      };
+    break;
+    case LOAD_SUCCESS:
+      newState.nodes[action.nodeId].data = action.result;
+      newState.nodes[action.nodeId].loading = false;
+      newState.nodes[action.nodeId].loaded = true;
+      newState.error = null;
+      return {
+        ...state,
+        newState
+      };
+    break;
+    case LOAD_FAIL:
+      newState.nodes[action.nodeId].data = null;
+      newState.nodes[action.nodeId].loaded = false;
+      newState.nodes[action.nodeId].loading = false;
+      newState.error = action.error;
+    break;
     default:
       return state;
+    break;
   }
 };
 
@@ -28,6 +62,14 @@ export function isExpanded(globalState, nodeId) {
 export function expand(nodeId) {
   return {
     type: EXPAND_NODE,
-    nodeId
+    nodeId: nodeId
   };
-}
+};
+
+export function load(nodeId) {
+  return {
+    types: [LOAD, LOAD_SUCCESS, LOAD_FAIL],
+    nodeId: nodeId,
+    promise: (client) => client.get('nodes/' + nodeId)
+  }
+};
